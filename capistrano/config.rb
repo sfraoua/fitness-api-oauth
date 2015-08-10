bdc00268e702f2d7729d141ca34458dc08f3971f
Selim F <sfraoua@gmail.com>
2015-08-10 23:36:35 +0200

    update mongo db conf

--- capistrano/config.rb
+++ capistrano/config.rb
@@ -13,7 +13,7 @@ set :use_sudo, true
 set :composer_install_flags, '--prefer-dist --no-interaction --optimize-autoloader'
 
 
-set :keep_releases, 3
+set :keep_releases, 4
 
 set :bower_flags, '--quiet --allow-root'
 
@@ -23,6 +23,7 @@ set :deletable_dirs, ["#{release_path}", "#{release_path}"]
 
 
 #=========== SF ==============
+# Symfony environment
 set :symfony_env,  "prod"
 
 # Symfony application path
@@ -44,29 +45,50 @@ set :app_config_path,       fetch(:app_path) + "/config"
 set :controllers_to_clear, ["app_*.php"]
 
 # Files that need to remain the same between deploys
-
 set :linked_files, %w{app/config/parameters.yml}
-set :linked_dirs, %w{app/logs web/uploads}
+
+# Dirs that need to remain the same between deploys (shared dirs)
+set :linked_dirs,           [fetch(:log_path), fetch(:web_path) + "/uploads"]
 
 set :shared_children,     ["app/logs", "web/uploads"]
+
 
 # Dirs that need to be writable by the HTTP Server (i.e. cache, log dirs)
 set :file_permissions_paths,         [fetch(:log_path), fetch(:cache_path)]
 
 # Name used by the Web Server (i.e. www-data for Apache)
-set :webserver_user,        "www-data"
+set :file_permissions_users, ['tannana', "www-data", "tanna"]
+
+# Name used by the Web Server (i.e. www-data for Apache)
+set :webserver_user,        "tannana"
 
 # Method used to set permissions (:chmod, :acl, or :chgrp)
-set :permission_method,     :acl
+set :permission_method,     false
 
 # Execute set permissions
-set :use_set_permissions,   true
+set :use_set_permissions,   false
+
+# Symfony console path
+set :symfony_console_path, fetch(:app_path) + "/console"
+
+# Symfony console flags
+set :symfony_console_flags, "--no-debug"
+
+# Assets install path
+set :assets_install_path,   fetch(:web_path)
+
+# Assets install flags
+set :assets_install_flags,  '--symlink'
+
+# Assetic dump flags
+set :assetic_dump_flags,  ''
+
+fetch(:default_env).merge!(symfony_env: fetch(:symfony_env))
 
 
 
+before 'deploy:updated', 'cache:fix_file_permissions'
 after 'deploy:updated', 'bower:install'
 after 'deploy:updated', 'workout:database'
 after 'deploy:finishing', 'app:copy_htaccess'
-after 'app:copy_htaccess', 'cache:fix_file_permissions'
-after 'deploy:finishing', 'deploy:cleanup'
-after 'deploy:cleanup', 'cache:create_cache_folder'
+after 'deploy:cleanup', 'cache:fix_file_permissions'
